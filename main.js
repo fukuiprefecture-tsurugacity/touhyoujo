2ab866bb1c2061a1d39078497404e2d820816e36
fukuiprefecture-tsurugacity <37329695+fukuiprefecture-tsurugacity@users.noreply.github.com>
2019-02-04 19:01:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function main() {
 	var lng = $.cookie('touhyoujyo_lng');
 	var zoom = $.cookie('touhyoujyo_zoom');
 	
-	var latlng = [35.737841, 139.653912];
+	var latlng = [35.645266, 136.055667];
 
 	if (lat != null && lng != null) {
 		latlng = [lat, lng];
